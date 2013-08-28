b0abdfa9b51816aecc85908ea4b9e429cda0d4e9
kelmer <kelmer@gmail.com>
2013-08-28 18:15:55 +0200

    Leyendo estructura

---
+++ AdventureRipper/Model/Files/FileEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventureRipper.Model
+{
+    class FileEntry
+    {
+        public string FileName { get; private set; }
+        public string FileOffset { get; private set; }
+        public static String Icon = "file.png";
+
+    }
+}
